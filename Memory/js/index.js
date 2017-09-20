7b946c19f2dcb0919c57633f88e8760a17ace3d8
MingShined <996578843@qq.com>
2017-09-20 09:34:29 +0800

    改进了细节

--- Memory/js/index.js
+++ Memory/js/index.js
@@ -1,6 +1,5 @@
 window.onload = function() {
     var show = document.getElementById('show');
-    var quit = document.getElementById('quit');
     var mask = document.getElementById('mask');
     var sidebar = document.getElementById('sidebar');
     var content = document.getElementById('content');
@@ -9,16 +8,14 @@ window.onload = function() {
     show.onclick = function() {
         mask.style.display = 'block';
         sidebar.style.transform = 'translate(0, 0)';
-        content.style.transform = 'translateX(80%)';
-        quit.style.left = '102%';
+        // content.style.transform = 'translateX(80%)';
     }
 
     // 侧边内容滑出
-    quit.onclick = function() {
+    mask.onclick = function() {
         mask.style.display = 'none';
         sidebar.style.transform = 'translateX(-100%)';
-        content.style.transform = 'translateX(0)';
-        quit.style.left = '0';
+        // content.style.transform = 'translateX(0)';
     }
 
     //朋友圈/附近tab切换
