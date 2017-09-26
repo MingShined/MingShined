a179e9eb02113e26f2f8ae0c1441f23d31575385
MingShined <996578843@qq.com>
2017-09-26 15:10:56 +0800

    version 1.0

--- weather/js/index.js
+++ weather/js/index.js
@@ -1,8 +1,9 @@
 function weather(data) {
     var d = data.weather;
+    var area = data.area[2][0];
     var oDate = document.getElementById('date');
-    var dayHTML = document.getElementById('day').innerHTML = ' ';
-    var nightHTML = document.getElementById('night').innerHTML = ' ';
+    // var dayHTML = document.getElementById('day').innerHTML = ' ';
+    // var nightHTML = document.getElementById('night').innerHTML = ' ';
     for (var i = 0; i < d.length - 4; i++) {
         // var day = document.getElementById('day');
         var date = d[i].date;
@@ -16,26 +17,27 @@ function weather(data) {
 
         //温度
         document.getElementById('temval').innerHTML = day[2] + '℃';
-
+        //地区
+        document.getElementById('cityName').innerHTML = area;
 
         // 白天
-        tag += '<li><i class="iconfont icon-tianqi">温度:</i>' + day[2] + '℃' + '</li>';
-        tag += '<li><i class="iconfont icon-tianqi">天气:</i>' + day[1] + '</li>';
-        tag += '<li><i class="iconfont icon-tianqi">风向:' + day[3] + '</i></li>'
-        tag += '<li><i class="iconfont icon-tianqi">风速:' + day[4] + '</i></li>'
-        var ul = document.createElement('ul');
-        ul.innerHTML = tag;
-        document.getElementById('day').appendChild(ul);
+        tag += '<li class="animated bounceInLeft"><i class="iconfont icon-wendu"></i>温度:' + day[2] + '℃' + '</li>';
+        tag += '<li class="animated bounceInLeft t2"><i class="iconfont icon-tianqi"></i>天气:' + day[1] + '</li>';
+        tag += '<li class="animated bounceInLeft t3"><i class="iconfont icon-fengxiang"></i>风向:' + day[3] + '</li>'
+        tag += '<li class="animated bounceInLeft t4"><i class="iconfont icon-fengsu"></i>风速:' + day[4] + '</li>'
+        var Dayul = document.createElement('ul');
+        Dayul.innerHTML = tag;
+        document.getElementById('day').appendChild(Dayul);
 
         //晚上
 
-        Tag += '<li><i class="iconfont icon-tianqi">温度:</i>' + night[2] + '℃' + '</li>';
-        Tag += '<li><i class="iconfont icon-tianqi">天气:</i>' + night[1] + '</li>';
-        Tag += '<li><i class="iconfont icon-tianqi">风向:' + night[3] + '</i></li>'
-        Tag += '<li><i class="iconfont icon-tianqi">风速:' + night[4] + '</i></li>'
-        var Ul = document.createElement('ul');
-        Ul.innerHTML = Tag;
-        document.getElementById('night').appendChild(Ul);
+        Tag += '<li class="animated bounceInRight">温度:' + night[2] + '℃' + '<i class="iconfont icon-wendu"></i></li>';
+        Tag += '<li class="animated bounceInRight t2">天气:' + night[1] + '<i class="iconfont icon-tianqi"></i></li>';
+        Tag += '<li class="animated bounceInRight t3">风向:' + night[3] + '<i class="iconfont icon-fengxiang"></i></li>'
+        Tag += '<li class="animated bounceInRight t4">风速:' + night[4] + '<i class="iconfont icon-fengsu"></i></li>'
+        var Nightul = document.createElement('ul');
+        Nightul.innerHTML = Tag;
+        document.getElementById('night').appendChild(Nightul);
 
     }
 }
