4e64828e13559d58db47e3356513ae8a94c9ef33
MingShined <996578843@qq.com>
2017-10-10 19:19:09 +0800

    新增轮播图插件和模板

---
+++ 重要源码/JS封装插件/scroll兼容.js
@@ -0,0 +1,21 @@
+function scroll() {
+        //检测是否支持pageYOffset对象
+        if (window.pageYOffset != null) {
+            return {
+                left: window.pageXOffset,
+                top: window.pageYOffset
+            }
+        }
+        //不是怪异模式时(怪异模式指的是没有DTD)
+        else if (document.compatMode == 'CSS1compat') {
+            return {
+                left: document.documentElement.scrollLeft,
+                top: document.documentElement.scrollTop
+            }
+        }
+        //怪异模式时
+        return {
+            left: document.body.scrollLeft,
+            top: document.body.scrollTop
+        }
+    }
