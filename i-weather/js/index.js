9ff3acda687d04eb63b78c5cde255608353d9cb9
MingShined <996578843@qq.com>
2017-12-03 22:47:48 +0800

    i-weather

---
+++ i-weather/js/index.js
@@ -0,0 +1,11 @@
+var swiper4 = new Swiper('#swiper4', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.swiper4-next',
+        prevEl: '.swiper4-prev',
+    },
+});
