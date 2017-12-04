b1b2399aa8a5cbecb58a58836c30eb4734d9f2ec
MingShined <996578843@qq.com>
2017-12-04 21:59:57 +0800

    梅花界面

--- i-weather/js/index.js
+++ i-weather/js/index.js
@@ -1,11 +1,38 @@
-var swiper4 = new Swiper('#swiper4', {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: '.swiper4-next',
-        prevEl: '.swiper4-prev',
-    },
-});
+/* JS Document 
+use for:www.18183.com
+version:1.0
+Data:2017/12/04
+Author: MingShined
+*/
+
+
+$(function() {
+
+
+	var tips = new Swiper('#tips',{
+		loop:true,
+		slidesPerView:1,
+		slidesPerGroup:1,
+		loopFillGroupWithBlank:true,
+		spaceBetween:10,
+		navigation:{
+			nextEl:'.swiper-next1',
+			prevEl:'.swiper-prev1',
+		},
+		// pagination:{}
+	})
+
+    var future = new Swiper('#future', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: '.swiper-next2',
+            prevEl: '.swiper-prev2',
+        },
+    });
+
+
+})
