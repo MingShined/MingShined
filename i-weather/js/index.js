06d91550dd8197e745be5bb9e3dede47b0d7b138
MingShined <996578843@qq.com>
2017-12-05 19:01:08 +0800

    天气接口更新

--- i-weather/js/index.js
+++ i-weather/js/index.js
@@ -20,9 +20,9 @@ $(function() {
         var minute = '';
         var timeVal = [];
         if (time.getMinutes() < 10) {
-        	minute = '0' + time.getMinutes();
-        }else {
-        	minute = time.getMinutes();
+            minute = '0' + time.getMinutes();
+        } else {
+            minute = time.getMinutes();
         }
         if (time.getHours() > 12) {
             $('.timeSign').text('PM');
@@ -46,31 +46,20 @@ $(function() {
         elem.text(val);
     }
 
-    //提醒事项滑屏
-    var tips = new Swiper('#tips', {
-        loop: true,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        loopFillGroupWithBlank: true,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: '.swiper-next1',
-            prevEl: '.swiper-prev1',
-        },
-    })
-
-    //未来五天天气滑屏
-    var future = new Swiper('#future', {
-        slidesPerView: 3,
-        spaceBetween: 10,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        navigation: {
-            nextEl: '.swiper-next2',
-            prevEl: '.swiper-prev2',
-        },
-    });
+    //     var swiper = new Swiper('#future', {  
+    //     loop : true,  
+    //     effect: 'coverflow',  
+    //     grabCursor: true,  
+    //     centeredSlides: true,  
+    //     slidesPerView: 'auto',  
+    //     coverflow: {  
+    //         rotate: 0,// 旋转的角度  
+    //         stretch: 100,// 拉伸   图片间左右的间距和密集度  
+    //         depth: 100,// 深度   切换图片间上下的间距和密集度  
+    //         modifier: 4,// 修正值 该值越大前面的效果越明显  
+    //         // slideShadows : true// 页面阴影效果  
+    //     }  
+    // });  
 
     //ajax获取数据
     //获取IP
@@ -99,9 +88,49 @@ $(function() {
                         })
                         .done(function(data) {
                             console.log(data)
+                            //分解json节点
                             var $weather = data.weather[0];
+                            var $now = $weather.now;
+                            var $today = $weather.today;
+                            //获取日期
                             var $date = $weather.last_update.substring(0, 10).replace(/-/g, "/");
                             $('.date').text($date);
+                            //获取城市名称和id
+                            $('.cityname').text($weather.city_name);
+                            $('.cityid').text($weather.city_id);
+                            //获取温度和天气字段
+                            var $codeImg = 'http://weixin.jirengu.com/images/weather/code/' + $now.code + '.png';
+                            $('.temp').text($now.temperature + '°').next().find('.codeimg').attr('src', $codeImg);
+                            $('.codetext').text('/ ' + $now.text);
+                            //获取详细信息
+                            var $infoName = [$now.wind_direction + '风', '风速', '湿度', '可见度', '日出', '日落'];
+                            var $infoVal = [$now.wind_scale + '级', $now.wind_speed, $now.humidity, $now.visibility, $today.sunset, $today.sunrise]
+                            var $suggessVal = [
+                                $today.suggestion.dressing,
+                                $today.suggestion.uv,
+                                $today.suggestion.car_washing,
+                                $today.suggestion.travel,
+                                $today.suggestion.flu,
+                                $today.suggestion.sport
+                            ]
+                            for (var i = 0; i < $('.info li').length; i++) {
+                                $('.info li').eq(i).text($infoName[i] + '/' + $infoVal[i]);
+                                $('#tips .tips-desc').eq(i).text($suggessVal[i].details);
+                                console.log($('#tips .tips-desc').eq(i + 1).html());
+                            }
+                            //提醒事项滑屏
+                            var tips = new Swiper('#tips', {
+                                loop: true,
+                                slidesPerView: 1,
+                                slidesPerGroup: 1,
+                                loopFillGroupWithBlank: true,
+                                spaceBetween: 10,
+                                navigation: {
+                                    nextEl: '.swiper-next1',
+                                    prevEl: '.swiper-prev1',
+                                },
+                            })
+
                         })
                         .fail(function() {
                             console.log("error");
@@ -115,6 +144,22 @@ $(function() {
         .fail(function() {
             console.log("error");
         })
+
+
+
+    // 未来五天天气滑屏
+    var future = new Swiper('#future', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: '.swiper-next2',
+            prevEl: '.swiper-prev2',
+        },
+    });
+
 
 
 })
