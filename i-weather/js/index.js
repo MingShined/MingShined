2e6dde97cdbfc73f3ae3a8f6a1d1dbce4bb19df7
MingShined <996578843@qq.com>
2017-12-05 22:30:32 +0800

    测试1.0

--- i-weather/js/index.js
+++ i-weather/js/index.js
@@ -109,6 +109,8 @@ $(function() {
                 var $weather = data.weather[0];
                 var $now = $weather.now;
                 var $today = $weather.today;
+                var $future = $weather.future;
+                console.log($future[1].code1)
                 //获取日期
                 var $date = $weather.last_update.substring(0, 10).replace(/-/g, "/");
                 $('.date').text($date);
@@ -116,10 +118,11 @@ $(function() {
                 $('.cityname').text($weather.city_name);
                 $('.cityid').text($weather.city_id);
                 //获取温度和天气字段
-                var $codeImg = 'http://weixin.jirengu.com/images/weather/code/' + $now.code + '.png';
+                var $codeImg = 'https://weixin.jirengu.com/images/weather/code/' + $now.code + '.png';
+                var $suggestionImg = '';
                 $('.temp').text($now.temperature + '°').next().find('.codeimg').attr('src', $codeImg);
                 $('.codetext').text('/ ' + $now.text);
-                //获取详细信息
+                //获取详细信息和未来六天天气数据
                 var $infoName = [$now.wind_direction + '风', '风速', '湿度', '可见度', '日出', '日落'];
                 var $infoVal = [$now.wind_scale + '级', $now.wind_speed, $now.humidity, $now.visibility, $today.sunset, $today.sunrise]
                 var $suggessVal = [
@@ -130,11 +133,20 @@ $(function() {
                     $today.suggestion.flu,
                     $today.suggestion.sport
                 ]
+                var $html = '';
                 for (var i = 0; i < $('.info li').length; i++) {
+                    $suggestionImg = 'https://weixin.jirengu.com/images/weather/code/' + $future[i].code2 + '.png';
                     $('.info li').eq(i).text($infoName[i] + '/' + $infoVal[i]);
                     $('#tips .tips-desc').eq(i).text($suggessVal[i].details);
-                    console.log($('#tips .tips-desc').eq(i + 1).html());
+                    $html += '<div class="swiper-slide"><ul>';
+                    $html += '<li>' + $future[i + 1].day + '</li>';
+                    $html += '<li>' + $future[i + 1].text + '</li>';
+                    $html += '<li><img src="' + $suggestionImg + '" /></li>';
+                    $html += '<li>' + $future[i + 1].low + '~' + $future[i].high + '</li>';
+                    $html += '</ul></div>';
                 }
+                $('#future .swiper-wrapper').append($html);
+
                 //提醒事项滑屏
                 var tips = new Swiper('#tips', {
                     loop: true,
@@ -147,29 +159,22 @@ $(function() {
                         prevEl: '.swiper-prev1',
                     },
                 })
+                // 未来六天天气滑屏
+                var future = new Swiper('#future', {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+                    slidesPerGroup: 1,
+                    loop: true,
+                    loopFillGroupWithBlank: true,
+                    navigation: {
+                        nextEl: '.swiper-next2',
+                        prevEl: '.swiper-prev2',
+                    },
+                });
             })
             .fail(function() {
                 console.log("error");
             })
     }
-
-
-
-
-
-    // 未来五天天气滑屏
-    var future = new Swiper('#future', {
-        slidesPerView: 3,
-        spaceBetween: 10,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        navigation: {
-            nextEl: '.swiper-next2',
-            prevEl: '.swiper-prev2',
-        },
-    });
-
-
 
 })
